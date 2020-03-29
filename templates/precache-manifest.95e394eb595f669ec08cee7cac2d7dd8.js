bc6650b9dd1373f7143913c578b2cf476b8cff86
Felix-Mutinda <1mutindafelix@gmail.com>
2020-03-29 12:32:27 +0300

    remove app core

---
+++ templates/precache-manifest.95e394eb595f669ec08cee7cac2d7dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689155435a72050f6861cfb5e4393ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2d9588e4ae505d94d6",
+    "url": "/static/css/main.8821e70f.chunk.css"
+  },
+  {
+    "revision": "074e2a0897c49f17b129",
+    "url": "/static/js/2.b2bbe2bc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b2bbe2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2d9588e4ae505d94d6",
+    "url": "/static/js/main.82c7e543.chunk.js"
+  },
+  {
+    "revision": "d9a5b028273c76556fc0",
+    "url": "/static/js/runtime-main.ef94caf0.js"
+  }
+]);
