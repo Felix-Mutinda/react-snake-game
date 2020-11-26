07302f5c10ebad47f71dc3a8dde0489bd4cfd725
Felix-Mutinda <1mutindafelix@gmail.com>
2020-11-26 11:58:07 +0300

    minor update

--- snake_game/urls.py
+++ snake_game/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('address/', views.address_from_postcode, name='address_from_postcode'),
+    # path('address/', views.address_from_postcode, name='address_from_postcode'),
 ]
--- snake_game/views.py
+++ snake_game/views.py
@@ -5,14 +5,14 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def address_from_postcode(request):
-    if request.method == 'GET':
-        postcode = request.GET.get('postcode', None)
-        address = 'Unknown Address'
-        if postcode == '48':
-            address = 'Wamunyu'
-        if postcode == '1':
-            address = 'Nairobi'
-        if postcode:
-            return HttpResponse(address)
-    return render(request, 'address_from_postcode.html')
+# def address_from_postcode(request):
+#     if request.method == 'GET':
+#         postcode = request.GET.get('postcode', None)
+#         address = 'Unknown Address'
+#         if postcode == '48':
+#             address = 'Wamunyu'
+#         if postcode == '1':
+#             address = 'Nairobi'
+#         if postcode:
+#             return HttpResponse(address)
+#     return render(request, 'address_from_postcode.html')
